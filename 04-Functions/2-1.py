8d28e2ce27b1f8341d020a10763783afbfbc6614
TymonKalista <tymonkalista@gmail.com>
2025-11-14 12:13:16 +0100

    aaa

---
+++ 04-Functions/2-1.py
@@ -0,0 +1,33 @@
+###
+# Program for testing built-in functions
+#
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+nuumber = int("20303")
+print('The number of characters in "computer science" is', str_length)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+
+x = input('WHAT LETTER YOU WANT: ')
+print('lETTER IS', x)
+
+binary = bin(304)
+print('Binary number of "304" is', binary)
+
+heximal = hex(304)
+print('Heximal number of "304" is', heximal)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+euro = chr(€)
+print('The chr  of Euro', euro)
+
+absoulte = abs(-17)
+print()
